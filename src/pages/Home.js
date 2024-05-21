a128736ba8f01dcac954ad05bf1d9c877d091585
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-05-21 10:29:19 +0500

    Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
       <h1>My Home Page</h1>
+      <p>
+        <Link to={"/products"}>Go to Products</Link>
+      </p>
     </div>
   );
 };
