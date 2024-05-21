2d3282a894995368f9de007cd7ed8fc1749795ff
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-05-21 18:39:57 +0500

    Defining & Using Dynamic Routes

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ProductDetailPage = () => {
+  const params = useParams();
+  return (
+    <div>
+      <h1>Product Detail!</h1>
+      <p>{params.productId}</p>
+    </div>
+  );
+};
+
+export default ProductDetailPage;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,6 +4,12 @@ const ProductsPage = () => {
   return (
     <div>
       <h1>My Products Page</h1>
+
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
     </div>
   );
 };
