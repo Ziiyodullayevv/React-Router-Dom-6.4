538657c9e57c69a81e7d4e96293d648d122fd602
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-05-21 11:50:58 +0500

    Working with Navigation Links (NavLink)

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  // active link
+  const active = ({ isActive }) => (isActive ? classes.active : undefined);
+
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink className={active} to="/" end>
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink className={active} to="/products">
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
