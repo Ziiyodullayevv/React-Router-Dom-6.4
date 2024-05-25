a7f8b4b223c3195436a9a7844ec694641802c5e7
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-05-25 11:17:21 +0500

    Extracting Error Data & Throwing Responses

---
+++ FRONTEND/src/pages/Error.js
@@ -0,0 +1,28 @@
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  let title = "An error occurred";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Page Not Found";
+    message = "Could not find Page";
+  }
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
+};
+
+export default ErrorPage;
--- FRONTEND/src/pages/Events.js
+++ FRONTEND/src/pages/Events.js
@@ -3,7 +3,8 @@ import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
   //events apidan malumot olib kelish uchun:
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <>
@@ -19,9 +20,10 @@ export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 };
