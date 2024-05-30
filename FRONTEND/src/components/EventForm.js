b8387c61e1554335e384118251eb1a8d4217e3d8
Ziiyodullayevv <akobirziyod2323@gmail.com>
2024-05-30 09:58:48 +0500

    Validating User Input & Outputting Validation Errors

--- FRONTEND/src/components/EventForm.js
+++ FRONTEND/src/components/EventForm.js
@@ -1,8 +1,15 @@
-import { Form, useNavigate, useNavigation } from "react-router-dom";
+import {
+  Form,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
+  console.log(data, "action");
   const navigate = useNavigate();
   const navigation = useNavigation();
 
@@ -13,6 +20,13 @@ function EventForm({ method, event }) {
 
   return (
     <Form method={"post"} className={classes.form}>
+      {data?.errors && (
+        <ul>
+          {Object.values(data.errors).map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
